438c6b10d4272040196b9af23fae1cea2c90bacf
Jan Hug <me@dulnan.net>
2018-09-29 20:29:49 +0200

    release 1.0.0

--- src/Catenary.js
+++ src/Catenary.js
@@ -1,16 +1,35 @@
+/**
+ * Given two points and a length, calculate and draw the catenary.
+ * 
+ * JavaScript implementation:
+ * Copyright (c) 2018 Jan Hug <me@dulnan.net>
+ * Released under the MIT license.
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Original ActionScript implementation:
+ * Copyright poiasd ( http://wonderfl.net/user/poiasd )
+ * MIT License ( http://www.opensource.org/licenses/mit-license.php )
+ * Downloaded from: http://wonderfl.net/c/8Bnl
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * Archived by and downloaded from:
+ * http://wa.zozuar.org/code.php?c=8Bnl
+ */
 import Point from './Point'
 
-const EPSILON = Number.EPSILON
+const EPSILON = 1e-6
 
 class Catenary {
   /**
    * constructor
    *
-   * @param {object} settings
-   * @param {(number|string)} settings.segments Number of segments of the chain. 'auto' will adjust segments in relation to chainLength
-   * @param {number} settings.iterationLimit Max amount iterations for getting catenary parameters
+   * @param {Object} settings
+   * @param {Number} settings.segments Number of segments of the chain.
+   * @param {Number} settings.iterationLimit Maximum amount iterations for getting catenary parameters.
    */
-  constructor ({ segments = 50, iterationLimit = 50 } = {}) {
+  constructor ({ segments = 50, iterationLimit = 100 } = {}) {
     this.p1 = new Point()
     this.p2 = new Point()
 
@@ -18,32 +37,40 @@ class Catenary {
     this.iterationLimit = iterationLimit
   }
 
-  drawToCanvas (context, p1, p2, chainLength) {
-    this.p1.update(p1)
-    this.p2.update(p2)
+  /**
+   * Draws a catenary given two coordinates, a length and a context.
+   * 
+   * @param {CanvasRenderingContext2D} context The canvas context to draw the catenary on to.
+   * @param {Point} p1 First point
+   * @param {Point} p2 Second point
+   * @param {Number} chainLength The length of the catenary
+   */
+  drawToCanvas (context, point1, point2, chainLength) {
+    this.p1.update(point1)
+    this.p2.update(point2)
 
-    return this.calculateCatenary(this.p1, this.p2, chainLength, context)
-  }
+    const isFlipped = this.p1.x > this.p2.x
 
-  calculateCatenary (point1, point2, chainLength, context) {
-    const isFlipped = point1.x > point2.x
-
-    const p1 = isFlipped ? point2 : point1
-    const p2 = isFlipped ? point1 : point2
+    const p1 = isFlipped ? this.p2 : this.p1
+    const p2 = isFlipped ? this.p1 : this.p2
 
     const distance = p1.getDistanceTo(p2)
 
     let curveData = []
     let isStraight = true
 
+    // Prevent "expensive" catenary calculations if it would only result
+    // in a straight line.
     if (distance < chainLength) {
       const diff = p2.x - p1.x
 
+      // If the distance on the x axis of both points is too small, don't
+      // calculate a catenary.
       if (diff > 0.01) {
-        let d = p2.x - p1.x
-        let h = p2.y - p1.y
-        let a = -this.getCatenaryParameter(d, h, chainLength, this.iterationLimit)
-        let x = (a * Math.log((chainLength + h) / (chainLength - h)) - d) * 0.5
+        let h = p2.x - p1.x
+        let v = p2.y - p1.y
+        let a = -this.getCatenaryParameter(h, v, chainLength, this.iterationLimit)
+        let x = (a * Math.log((chainLength + v) / (chainLength - v)) - h) * 0.5
         let y = a * Math.cosh(x / a)
         let offsetX = p1.x - x
         let offsetY = p1.y - y
@@ -75,21 +102,41 @@ class Catenary {
     return curveData
   }
 
-  getCatenaryParameter (d, h, length, limit) {
-    let m = Math.sqrt(length * length - h * h) / d
+  /**
+   * Determines catenary parameter.
+   * 
+   * @param {Number} h Horizontal distance of both points.
+   * @param {Number} v Vertical distance of both points.
+   * @param {Number} length The catenary length.
+   * @param {Number} limit Maximum amount of iterations to find parameter.
+   */
+  getCatenaryParameter (h, v, length, limit) {
+    let m = Math.sqrt(length * length - v * v) / h
     let x = Math.acosh(m) + 1
     let prevx = -1
     let count = 0
 
-    while (Math.abs(x - prevx) > 0 && count < limit) {
+    while (Math.abs(x - prevx) > EPSILON && count < limit) {
       prevx = x
       x = x - (Math.sinh(x) - m * x) / (Math.cosh(x) - m)
       count++
     }
 
-    return d / (2 * x)
+    return h / (2 * x)
   }
 
+  /**
+   * Calculate the catenary curve.
+   * Increasing the segments value will produce a catenary closer
+   * to reality, but will require more calcluations.
+   * 
+   * @param {Number} a The catenary parameter.
+   * @param {Point} p1 First point
+   * @param {Point} p2 Second point
+   * @param {Number} offsetX The calculated offset on the x axis.
+   * @param {Number} offsetY The calculated offset on the y axis.
+   * @param {Number} segments How many "parts" the chain should be made of.
+   */
   getCurve (a, p1, p2, offsetX, offsetY, segments) {
     let data = [p1.x, a * Math.cosh((p1.x - offsetX) / a) + offsetY]
 
@@ -107,12 +154,27 @@ class Catenary {
     return data
   }
 
+  /**
+   * Draws a straight line between two points.
+   *
+   * @param {Array} data Even indices are x, odd are y.
+   * @param {CanvasRenderingContext2D} context The context to draw to.
+   */
   drawLine (data, context) {
     context.moveTo(data[0][0], data[0][1])
 
     context.lineTo(data[1][0], data[1][1])
   }
 
+  /**
+   * Draws a quadratic curve between every calculated catenary segment,
+   * so that the segments don't look like straight lines.
+   *
+   * @param {Array} data Even indices are x, odd are y.
+   * @param {CanvasRenderingContext2D} context The context to draw to.
+   * 
+   * @returns {Array} The original segment coordinates.
+   */
   drawCurve (data, context) {
     let length = data.length * 0.5 - 1
     let ox = data[2]
